0efab9358520226696169c6dd2c0251d96721ef0
ViorelChirila <viorelchirila21@yahoo.ro>
2024-05-10 17:29:52 +0300

    Added simple splash screen with splash screen api

--- app/src/main/java/com/example/planificatorbuget/MainActivity.kt
+++ app/src/main/java/com/example/planificatorbuget/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.planificatorbuget
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +14,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.planificatorbuget.ui.theme.PlanificatorBugetTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val  viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                !viewModel.isReady.value
+            }
+            setOnExitAnimationListener{ screen->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.6f,
+                    0.0f
+                )
+
+                zoomX.interpolator = OvershootInterpolator()
+                zoomX.duration = 500L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.6f,
+                    0.0f
+                )
+
+                zoomY.interpolator = OvershootInterpolator()
+                zoomY.duration = 500L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+            }
+        }
         setContent {
             PlanificatorBugetTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/example/planificatorbuget/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.planificatorbuget
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel(){
+    private val _isReady = MutableStateFlow(false)
+    val isReady = _isReady
+
+    init {
+        viewModelScope.launch {
+            delay(3000L)
+            _isReady.value = true
+        }
+    }
+}
